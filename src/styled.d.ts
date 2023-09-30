944c48b88b3830a50bd14bafba8681e2822b938d
sanghee01 <sang6299@naver.com>
2023-09-30 14:36:59 +0900

    study: TS + Themes

---
+++ src/styled.d.ts
@@ -0,0 +1,10 @@
+import "styled-components";
+
+// 테마가 어떻게 보일지를 설명할 부분
+declare module "styled-components" {
+  export interface DefaultTheme {
+    textColor: string;
+    bgColor: string;
+    btnColor: string;
+  }
+}
