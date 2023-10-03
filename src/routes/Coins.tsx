35693ae88eb6c36865dcd173a869e8418935360a
sanghee01 <sang6299@naver.com>
2023-10-03 14:33:25 +0900

    study: styled-reset

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,10 @@
-const Coins = () => {
-  return <h1>Coins</h1>;
-};
+import styled from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+
+function Coins() {
+  return <Title>코인</Title>;
+}
 export default Coins;
